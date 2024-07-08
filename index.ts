af0ff271ef2b706c172a5c215f0ca92596bf4b5c
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2024-07-08 22:44:35 +0530

    v1.0.0

---
+++ index.ts
@@ -0,0 +1,123 @@
+import {
+  Mark,
+  markInputRule,
+  markPasteRule,
+  mergeAttributes,
+} from "@tiptap/core";
+
+export interface HighlightOptions {
+  multicolor: boolean;
+  HTMLAttributes: Record<string, any>;
+}
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    highlight: {
+      setHighlight: (attributes?: { color: string; id: string }) => ReturnType;
+      toggleHighlight: (attributes?: { color: string }) => ReturnType;
+      unsetHighlight: () => ReturnType;
+    };
+  }
+}
+
+export const inputRegex = /(?:^|\s)(==(?!\s+==)((?:[^=]+))==(?!\s+==))$/;
+
+export const pasteRegex = /(?:^|\s)(==(?!\s+==)((?:[^=]+))==(?!\s+==))/g;
+
+export const Highlight = Mark.create<HighlightOptions>({
+  name: "highlight",
+
+  addOptions() {
+    return {
+      multicolor: false,
+      HTMLAttributes: {},
+    };
+  },
+
+  addAttributes() {
+    if (!this.options.multicolor) {
+      return {};
+    }
+
+    return {
+      color: {
+        default: null,
+        parseHTML: (element) =>
+          element.getAttribute("data-color") || element.style.backgroundColor,
+        renderHTML: (attributes) => {
+          if (!attributes.color) {
+            return {};
+          }
+          return {
+            "data-color": attributes.color,
+            id: attributes.id,
+            style: `background-color: ${attributes.color}; color: inherit`,
+          };
+        },
+      },
+      id: {
+        default: null,
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "mark",
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "mark",
+      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+      0,
+    ];
+  },
+
+  addCommands() {
+    return {
+      setHighlight:
+        (attributes) =>
+        ({ commands }) => {
+          return commands.setMark(this.name, attributes);
+        },
+      toggleHighlight:
+        (attributes) =>
+        ({ commands }) => {
+          return commands.toggleMark(this.name, attributes);
+        },
+      unsetHighlight:
+        () =>
+        ({ commands }) => {
+          return commands.unsetMark(this.name);
+        },
+    };
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      "Mod-Shift-h": () => this.editor.commands.toggleHighlight(),
+    };
+  },
+
+  addInputRules() {
+    return [
+      markInputRule({
+        find: inputRegex,
+        type: this.type,
+      }),
+    ];
+  },
+
+  addPasteRules() {
+    return [
+      markPasteRule({
+        find: pasteRegex,
+        type: this.type,
+      }),
+    ];
+  },
+});
